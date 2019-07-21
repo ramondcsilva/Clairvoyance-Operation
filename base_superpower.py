74a754af1f2b6b2f1fb19f7c421f80a72670836d
Adlla Katarine <adllakatarine@hotmail.com>
2019-07-21 02:12:51 -0300

    Update base_superpower.py

--- base_superpower.py
+++ base_superpower.py
@@ -6,8 +6,8 @@ Created on Fri Jul  5 00:01:09 2019
 """
 
 import pandas as pd
+import pandas as dd
 from scipy.spatial import distance
-import numpy as np 
 
 # Leitura de arquivo para criação da base_herois de dados
 base_superpower = pd.read_csv('superpoderes.csv')
@@ -15,46 +15,40 @@ base_superpower = pd.read_csv('superpoderes.csv')
 previsores = base_superpower.iloc[:,1:169].values
 previsores = base_superpower.iloc[:,0:169].values
 
+
 ##################################################################
 ########################## DISTÂNCIA #############################
 ##################################################################
 
-#FUNCIONA
-distance.hamming([previsores[5,0:2]],[previsores[6,0:2]]) #ok
-distance.jaccard([previsores[5,0:2]],[previsores[6,0:2]]) #ok
-distance.kulsinski([previsores[5,0:2]],[previsores[6,0:2]]) #ok
-distance.rogerstanimoto([previsores[5,0:2]],[previsores[6,0:2]]) #ok
-distance.russellrao([previsores[5,0:2]],[previsores[6,0:2]]) #ok
-distance.sokalmichener([previsores[5,0:2]],[previsores[6,0:2]]) #ok
 
+colunas = [
+    'names',
+    'distance'
+]
 
-##################################################################
-################### EXEMPLO DE BUSCA #############################
-##################################################################
-
-#distance.hamming([previsores[4,[0,1,2, 10, 22, 35, 45, 57, 89, 99, 100, 121, 130]]],[previsores[7,[0,1,2, 10, 22, 35, 45, 57, 89, 99, 100, 121, 130]]])
-    
+base_distancias = dd.DataFrame(columns=colunas, index=range(667))
 
 #usuário faz a escolha do heroi (por indice)
 heroi = int(input('heroi escolhido: '))
 
-#um array é instanciado para guardar todos os valores de distancia calculados
-valor_distancias = np.zeros((667,1), dtype=np.double)
 
 #é feito um for para calcular a distancia entre todos os super herois e seus super-poderes
-#escolhido pelo usuário
+#escolhido pelo usuário     
 for i in range(0, 667):
-
-        #valor_distancias[i,0] = distance.hamming([previsores[4,[0,1,2, 10, 22, 35, 45, 57, 89, 99, 100,
-         #               121, 130]]],[previsores[i,[0,1,2, 10, 22, 35, 45, 57, 89, 99, 100, 121, 130]]])
-         valor_distancias[i,0] = distance.hamming([previsores[heroi,:]],[previsores[i,:]])
-
-#help(valor_distancias.sort) 
-         
+         base_distancias.loc[i,"distance"] = distance.hamming([previsores[heroi,:]],[previsores[i,:]])
 #Ordena o array em ordem crescente
-valor_distancias.sort(axis=0)
+base_distancias = base_distancias.sort_values(by=['distance'], axis = 0, kind= 'mergesort')
 
 
+#distance.hamming([previsores[4,[0,1,2, 10, 22, 35, 45, 57, 89, 99, 100, 121, 130]]],[previsores[7,[0,1,2, 10, 22, 35, 45, 57, 89, 99, 100, 121, 130]]])
+
+#FUNCIONA
+distance.hamming([previsores[5,1:3]],[previsores[6,1:3]]) #ok 0
+distance.jaccard([previsores[5,1:3]],[previsores[6,1:3]]) #ok 0
+distance.kulsinski([previsores[5,1:3]],[previsores[6,1:3]]) #ok 1
+distance.rogerstanimoto([previsores[5,1:3]],[previsores[6,1:3]]) #ok 0
+distance.russellrao([previsores[5,1:3]],[previsores[6,1:3]]) #ok 1
+distance.sokalmichener([previsores[5,1:3]],[previsores[6,1:3]]) #ok 0
 
 #NÃO FUNCIONA
 distance.dice([previsores[5,0:2]],[previsores[6,0:2]])
