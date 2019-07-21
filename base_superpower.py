512b01d03ad7190354e3c9fe64e7aa71ec390f31
Ramon Silva <ramondecerqueirasilva@gmail.com>
2019-07-21 02:53:50 -0300

    Update base_superpower.py

--- base_superpower.py
+++ base_superpower.py
@@ -50,14 +50,16 @@ valor_distancias = np.zeros((667,2), dtype=np.double)
 for i in range(0, 667):
         #valor_distancias[i,0] = distance.hamming([previsores[4,[0,1,2, 10, 22, 35, 45, 57, 89, 99, 100,
          #               121, 130]]],[previsores[i,[0,1,2, 10, 22, 35, 45, 57, 89, 99, 100, 121, 130]]])
-         valor_distancias[i,1] = distance.hamming([previsores[heroi,:]],[previsores[i,:]])
-         valor_distancias[i,0] = i
+     valor_distancias[i,1] = distance.hamming([previsores[heroi,:]],[previsores[i,:]])
+     valor_distancias[i,0] = i
 #help(valor_distancias.sort) 
 #Ordena o array em ordem crescente
 valor_distancias = pd.DataFrame(valor_distancias).sort_values(by=1)         
 # valor_distancias[:,1].sort(axis=0)
 # Guarda os 10 herois com menores distancias
 result = valor_distancias.iloc[1:11,0:2]
+# result = result.iloc[1:11,0:2]
+        
 '''
 #NÃO FUNCIONA
 distance.dice([previsores[5,0:2]],[previsores[6,0:2]])
